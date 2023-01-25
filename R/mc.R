8d82d6538aa3c164f6d6f26aa9b3de10a90ed0c4
Carl <cboettig@gmail.com>
2023-01-25 23:23:13 +0000

    better handling of messages

--- R/mc.R
+++ R/mc.R
@@ -2,20 +2,27 @@
 #' 
 #' The MINIO Client
 #' 
-#' @param command text string of an mc command (starting after the mc ...)
-#' @param ... additional arguments to `[processx::run]`
+#' @param command space-delimited text string of an mc command (starting after the mc ...)
+#' @param ... additional arguments to [processx::run()]
 #' @param path location where mc executable will be installed. By default will
 #' use the OS-appropriate storage location.  
-#' @return pid for the running processx process, invisibly.
+#' @param verbose print output?
+#' @return [processx::run()] list, with components `status`, `stdout`,
+#'  `stderr`, and `timeout`.
 #' @export 
 #' @details see <https://docs.min.io/docs/minio-client-quickstart-guide.html>
 
 # FIXME consider using processx -- why doesn't it work?
-mc <- function(command, ..., path = bin_path()) {
+mc <- function(command, ..., path = bin_path(), verbose =TRUE) {
   binary <- fs::path(path, "mc")
   args <- strsplit(command, split = " ")[[1]]
-  pid <- processx::run(binary, args, ...)
-  invisible(pid)
+  p <- processx::run(binary, args, ...)
+  
+  if(p$timeout & verbose) warning(paste("request", command, "timed out"))
+  if(p$status != 0) stop(paste(p$stderr))
+  
+  if(verbose) message(glue::glue(p$stdout))
+  invisible(p)
 }
 
 
